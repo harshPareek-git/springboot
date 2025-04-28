9f58fda332b30bba18a36dd9800c785cd0e46ba9
harshPareek-git <harshpareek.git@gmail.com>
2025-04-28 19:10:13 +0530

    code

--- src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -7,6 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -40,11 +41,17 @@ public class JournalEntryService {
         return journalEntryRepository.findById(String.valueOf(id));
     }
 
+    @Transactional
     public void deleteById(ObjectId id,String userName){
-        User user = userService.findByUserName(userName);
-        user.getJournalEntries().removeIf(x -> x.getId().equals(id));
-        userService.saveEntry(user);
-        journalEntryRepository.deleteById(String.valueOf(id));
+        try {
+            User user = userService.findByUserName(userName);
+            user.getJournalEntries().removeIf(x -> x.getId().equals(id));
+            userService.saveEntry(user);
+            journalEntryRepository.deleteById(String.valueOf(id));
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new RuntimeException("Exception while deleting JournalEntry",e);
+        }
     }
 
 }
