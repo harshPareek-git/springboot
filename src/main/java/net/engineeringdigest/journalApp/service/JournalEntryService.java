f94afbfb77d6712ab1d7b84f56ed6dad4814e6ae
harshPareek-git <harshpareek.git@gmail.com>
2025-05-11 11:00:59 +0530

    added logging in project

--- src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -1,9 +1,12 @@
 package net.engineeringdigest.journalApp.service;
 
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.entity.JournalEntry;
 import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -13,22 +16,31 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @Component
 public class JournalEntryService {
     @Autowired
     private JournalEntryRepository journalEntryRepository;
 
+//    private static final Logger logger = LoggerFactory.getLogger(JournalEntryService.class);
+
+
     @Autowired
     private UserService userService;
 
     @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
+        try {
         User user = userService.findByUserName(userName);
         journalEntry.setDate(LocalDateTime.now());
         JournalEntry saved = journalEntryRepository.save(journalEntry);
         user.getJournalEntries().add(saved);
         userService.saveUser(user);
+        } catch (Exception e) {
+            log.error("Exception occurred", e);
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveEntry(JournalEntry journalEntry){
--- src/main/java/net/engineeringdigest/journalApp/service/UserService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/UserService.java
@@ -3,6 +3,8 @@ package net.engineeringdigest.journalApp.service;
 import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,6 +24,8 @@ public class UserService  {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    private static final Logger logger = LoggerFactory.getLogger(JournalEntryService.class);
+
 //    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 //
     public void saveUser(User user){
@@ -29,9 +33,14 @@ public class UserService  {
     }
 
     public void saveNewUser(User user){
+        try {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRoles(Arrays.asList("USER"));
         userRepository.save(user);
+        }catch (Exception e){
+            logger.error("Exception occurred for {}",user.getUserName(),e);
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllEntries(){
